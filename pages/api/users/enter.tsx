182df36fce61437611d5d21d84030cc15445bae1
kangkyeongseo <kks_big@naver.com>
2022-09-26 13:58:11 +0900

    Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,17 +5,23 @@ import client from "@libs/server/client";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
   /*  if (email) {
     user = await client.user.findUnique({
       where: {
@@ -56,6 +62,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     }
     console.log(user);
   } */
-  return res.json({ ok: true });
+  return res.status(200).end();
 }
 export default withHandler("POST", handler);
