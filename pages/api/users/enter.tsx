5b166a98698a5d3e4604c7b6d762756b7333821c
kangkyeongseo <kks_big@naver.com>
2022-09-27 12:52:36 +0900

    Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,24 @@
 import twilio from "twilio";
+import nodemailer from "nodemailer";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { IResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import { error } from "console";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+const mailTransporter = nodemailer.createTransport({
+  service: "Naver",
+  host: "smtp.naver.com",
+  port: 587,
+  auth: {
+    user: process.env.USEREMAIL,
+    pass: process.env.MAISSPASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 async function handler(
   req: NextApiRequest,
@@ -35,6 +50,27 @@ async function handler(
       to: process.env.PHONE!,
       body: `Your login token is ${payload}`,
     });
+  }
+  if (email) {
+    const mailOptions = {
+      from: process.env.USEREMAIL,
+      to: email,
+      subject: "Carrot Market Authentication Email",
+      text: `Your login token is ${payload}`,
+    };
+    const message = await mailTransporter.sendMail(
+      mailOptions,
+      (error, response) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(response);
+          return null;
+        }
+      }
+    );
+    mailTransporter.close();
     console.log(message);
   }
   return res.json({
