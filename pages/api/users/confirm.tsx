39b6a7eda9a4848561a053d40574eb8367613c76
kangkyeongseo <kks_big@naver.com>
2022-09-28 17:07:15 +0900

    Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { IResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
@@ -6,8 +7,23 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponseType>
 ) {
+  console.log(req.session);
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "28347509234869823459023409823487509823478502783485203948523845293459023409512312314123123123342085237459872340958234950",
+});
