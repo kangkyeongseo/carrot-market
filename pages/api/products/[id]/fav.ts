a4d0325c2a080d170ecebb7c4c7698c19717f8fb
kangkyeongseo <kks_big@naver.com>
2022-10-12 17:22:37 +0900

    Favorite Products

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { IResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+import products from "..";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: Number(id),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExists) {
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: Number(id),
+          },
+        },
+      },
+    });
+  }
+  res.json({ ok: true });
+}
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
