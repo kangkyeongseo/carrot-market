31c57dfe350d414e011cc6aa409948306d2305b4
kangkyeongseo <kks_big@naver.com>
2022-10-21 17:10:49 +0900

    Stream and Message Model

--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -22,7 +22,7 @@ const Stream: NextPage = () => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
               strokeLinecap="round"
